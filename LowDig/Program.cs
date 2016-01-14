fb9d1ba7319c9abe1a010c9eaf4b558bba9ffd91
Casper <blue@pnda.me>
2016-01-14 02:02:28 +0100

    Moved settings to seperate class
    I like it when stuff looks neat so I moved settings block from
    Program.cs to its own class

--- LowDig/Program.cs
+++ LowDig/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Threading;
-using Newtonsoft.Json;
-using System.IO;
 using System;
 
 namespace LowDig
@@ -15,55 +13,6 @@ namespace LowDig
 
 
         /// <summary>
-        /// Reads the settings
-        /// </summary>
-        /// <returns>Returns false if no file found</returns>
-        private static bool ReadSettings()
-        {
-            string settingsPath = "Settings.json";
-            if (!File.Exists(settingsPath))
-            {
-                /*File doesn't exist, we'll create it here*/
-                var settingsClass = new Config.Settings()
-                {
-                    startId = 5000,
-                    endId = 10000,
-                    threadCount = 3
-                };
-
-                /*Write file*/
-                string djson = JsonConvert.SerializeObject(settingsClass, Formatting.Indented);
-                File.WriteAllText("Settings.json", djson);
-                Console.WriteLine("Settings.json has been written. Go edit it.");
-                Thread.Sleep(1500);
-            }
-            else
-            {
-                /*File exist, so we'll parse the information here*/
-                string setJson = File.ReadAllText("Settings.json");
-                if (!string.IsNullOrWhiteSpace(setJson))
-                {
-                    try
-                    {
-                        mSettings = JsonConvert.DeserializeObject<Config.Settings>(setJson);
-                        return true;
-                    }
-                    catch (JsonException jEx)
-                    {
-                        /*Incorrect format probably*/
-                        Console.WriteLine("Error parsing Settings.json");
-                        Console.WriteLine(jEx.ToString());
-                        Thread.Sleep(5000);
-                    }
-                }
-            }
-
-
-            return false;
-        }
-
-
-        /// <summary>
         /// Main functions
         /// Set settings and start session
         /// </summary>
@@ -71,7 +20,10 @@ namespace LowDig
         static void Main(string[] args)
         {
             /*Read settings*/
-            if (!ReadSettings())
+            mSettings = Settings.Read();
+
+            /*Settings was not read, return*/
+            if (mSettings == null)
                 return;
 
             /*Start session*/
@@ -80,6 +32,7 @@ namespace LowDig
             /*Keep us alive*/
             while (true)
             {
+                /*Update title with number of account found from session*/
                 Console.Title = string.Format("Zute | LogDig | Accounts found: {0}", mSession.mAccountsFound);
                 Thread.Sleep(1000);
             }
---
+++ LowDig/Settings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LowDig
+{
+    static class Settings
+    {
+        /// <summary>
+        /// Read or write settings for application
+        /// </summary>
+        /// <returns>Returns null if settings was not read</returns>
+        public static Config.Settings Read()
+        {
+            if (File.Exists("Settings.json"))
+            {
+                /*File exist, so we'll parse the information here*/
+                string setJson = File.ReadAllText("Settings.json");
+                if (!string.IsNullOrWhiteSpace(setJson))
+                {
+                    try
+                    {
+                        /*Return deserialized json string*/
+                        return JsonConvert.DeserializeObject<Config.Settings>(setJson);
+                    }
+                    catch (JsonException jEx)
+                    {
+                        /*Incorrect format probably*/
+                        Console.WriteLine("Error parsing Settings.json\n{0}", jEx.ToString());
+                        Thread.Sleep(1500);
+                    }
+                }
+            }
+            else
+            {
+                /*File doesn't exist, we'll create it here*/
+                var settingsClass = new Config.Settings()
+                {
+                    /*Temp values*/
+                    startId = 5000,
+                    endId = 10000,
+                    threadCount = 5
+                };
+
+                /*Write file*/
+                string djson = JsonConvert.SerializeObject(settingsClass, Formatting.Indented);
+                Console.WriteLine("Settings.json has been written. Edit the settings and launch the program again.");
+                File.WriteAllText("Settings.json", djson);
+                Thread.Sleep(1500);
+            }
+            
+            return null;
+        }
+    }
+}
