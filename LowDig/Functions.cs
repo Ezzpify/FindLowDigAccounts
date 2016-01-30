1f4857593dd3524b869e637f4e13d36eff6f059d
Matthew <matthewwc@riseup.net>
2016-01-30 16:14:39 -0500

    variable e was created but never used

--- LowDig/Functions.cs
+++ LowDig/Functions.cs
@@ -53,7 +53,7 @@ namespace LowDig
                  @"^(?("")("".+?(?<!\\)""@)|(([a-z]((\.(?!\.))|[-\w])*)(?<=[0-9a-z])@))" +
                  @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
                  RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            } catch (RegexMatchTimeoutException e)
+            } catch (RegexMatchTimeoutException)
             {
                 return false;
             }
